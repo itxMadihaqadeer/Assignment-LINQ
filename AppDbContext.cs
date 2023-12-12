5698fe161c765d187355a043df85e3a22eb27d64
itxMadihaqadeer <134267849+itxMadihaqadeer@users.noreply.github.com>
2023-12-12 10:33:37 -0800

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbCwithLINKQ.Model
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Class> Class { get; set; }
+        public DbSet<Faculty> Faculty { get; set; }
+        public DbSet<Enrolled> Enrolled { get; set; }
+
+    }
+
+    }
+
+
+       
+    
+    
---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DbCwithLINKQ.Model
+{
+    public class Student
+    {
+        [Key]
+        public int Sid { get; set; }
+        public string Sname { get; set; }
+        public string Major { get; set; }
+        public int Standing { get; set; }
+        // Additional properties...
+
+        // Navigation property
+        public List<Enrolled> Enrollments { get; set; }
+
+    }
+}
